f824d2a0fdd12c8fab4f01679022ea02bffa94d6
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-07-18 14:45:58 +0530

    Initial building of test case for typewise-alert.c

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,30 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+int dummy_temp[3][3] = { {-1,5,36}, {-2,13,47}, {-3,40,45} };
+
+TEST_CASE("Test Check and Alert") 
+{
+  test_checkAndAlert();
+}
+
+void test_checkAndAlert(void)
+{
+  AlertTarget       alertTarget;
+  BatteryCharacter  batteryChar;
+  double            temperatureInC;
+  CoolingType       coolType;
+  int               ii, jj;
+  
+  batteryChar.brand = "BOSCH";
+  alertTarget = TO_MAIL;
+  for(ii=0; ii < coolType.MED_ACTIVE_COOLING; ii++)
+  {
+    batteryChar.coolingType =   ii;
+    for(jj=0; jj < 3; jj++)
+    {
+      temperatureInc = dummy_temp[ii][jj];
+      checkAndAlert(alertTarget, batteryChar, temperatureInc);
+    }
+  }
 }
