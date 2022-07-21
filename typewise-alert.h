e2a4b78383b7301a7ec3c379cf62fd123badbae9
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-07-21 18:41:00 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -31,8 +31,3 @@ bool checkAndAlert(
 void sendAlert( AlertTarget alertTarget, BreachType breachType);
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
-void checkLowLimitBreach(double value, double lowerLimit, BreachType* breach);
-void checkUpLimitBreach(double value, double upperLimit, BreachType* breach);
-void evaluateLimit_PASSIVE_COOLING(CoolingType coolingType, int* lowerLimit, int* upperLimit);
-void evaluateLimit_HI_ACTIVE_COOLING(CoolingType coolingType, int* lowerLimit, int* upperLimit);
-void evaluateLimit_MED_ACTIVE_COOLING(CoolingType coolingType, int* lowerLimit, int* upperLimit);
