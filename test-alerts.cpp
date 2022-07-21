e947c4e59e374fa901b945f57e2700d745ad00e5
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-07-21 18:13:48 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -40,17 +40,17 @@ TEST_CASE("Test checkAndAlert functionality")
   BatteryCharacter batteryChar;
   
   batteryChar.coolingType = PASSIVE_COOLING;
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-1) == 1);
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,20) == 0);
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,36) == 1);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,-1) == 1);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,20) == 0);
+  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar,36) == 1);
   
   batteryChar.coolingType = HI_ACTIVE_COOLING;
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,-1) == 1);
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,20) == 0);
-  REQUIRE(checkAndAlert(TO_EMAIL,batteryChar,46) == 1);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar,-1) == 1);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar,20) == 0);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar,46) == 1);
   
   batteryChar.coolingType = MED_ACTIVE_COOLING;
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-1) == 1);
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,20) == 0);
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,42) == 1);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar,-1) == 1);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar,20) == 0);
+  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar,42) == 1);
   }
