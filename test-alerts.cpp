8602f40a95937205a41e3f2c59af74ac22882c11
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-07-21 18:03:38 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -41,6 +41,16 @@ TEST_CASE("Test checkAndAlert functionality")
   
   batteryChar.coolingType = PASSIVE_COOLING;
   REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-1) == 1);
-  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,20) == 1);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,20) == 0);
   REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,36) == 1);
+  
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-1) == 1);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,20) == 0);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,46) == 1);
+  
+  batteryChar.coolingType = MED_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,-1) == 1);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,20) == 0);
+  REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,42) == 1);
   }
